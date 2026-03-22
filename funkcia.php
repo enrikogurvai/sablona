ec436905a4f94ccf789cbbbee9773b41a6eba22f
enrikogurvai <enriko.gurvai@gmail.com>
2026-03-22 15:43:54 +0100

    Dal som toho menej aby toho bol viac

---
+++ funkcia.php
@@ -0,0 +1,30 @@
+<?php
+
+function vygenerujPortfolio($dir) {
+    $files = array_merge(
+        glob($dir . "/*.jpg"),
+    );
+    $json = file_get_contents(__DIR__ . "../portfolio.json");
+    $data = json_decode($json, true);
+
+    $i = 0;
+    echo '<div class="row">';
+    foreach ($files as $file) {
+        $filename = basename($file);
+
+        echo '<div class="col-25 portfolio">';
+        echo '<img src="' . $file . '" alt="' . ($data[$filename] ?? "Bez názvu") . '">';
+        echo '<div class="portfolio-text">';
+        echo $data[$filename] ?? "Bez názvu";
+        echo '</div>';
+
+        echo '</div>';
+
+        $i++;
+        if ($i % 4 == 0 && $i != 0) {
+            echo '</div><div class="row">';
+        }
+    }
+
+    echo '</div>';
+}
